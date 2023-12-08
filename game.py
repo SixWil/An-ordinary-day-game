ac5edff248fd00a3c485badf0475088db150ee59
SixWil <sixten.wilde@elev.ga.ntig.se>
2023-12-08 22:53:51 +0100

    Köket färdigt

--- game.py
+++ game.py
@@ -14,20 +14,34 @@ def reset():
     global frukost
     frukost = ""
 
+    global dagsplan
+    dagsplan = ""
+
+    global droger
+    droger = []
+
+    global tunnan
+    tunnan = ""
+
+    global spelare
+    spelare = []
+
 #Start
 försök = 0
+
 def start():
-    reset()
-    os.system('cls')
+    while True:
+        reset()
+        os.system('cls')
 
-    global försök
-    försök = försök + 1
-    print(f"""
-    AN ORDINARY DAY
-    försök {försök}
-    """)
+        global försök
+        försök = försök + 1
+        print(f"""
+        AN ORDINARY DAY
+        försök {försök}
+        """)
 
-    sovrum()
+        sovrum()
 
 # sovrum
 def sovrum():
@@ -96,8 +110,83 @@ def sovrum():
 # köket
 def köket():
     global frukost
-    frukost = input("sample text: köket")
-            
+    frukost = input("""
+    I köket står en svart tunna med en toxic waste symbol på,
+    den osar en grön gas.
+    bakom ugnen ligger en macka som varit där så länge du kan minnas.
+    Ur skafferiet nästan lyser ett fantastiskt paket med smaskiga cornflakes från kellogs.
+    vad gör du för frukost?
+    för:              skriv:
+    sniffa på tunnan:   1
+    Mackan:             2
+    kellogs corflakes™: 3
+    """)
+    if frukost == "2":
+        print("Mycket dåligt val")
+        hallen()
+
+    if frukost == "3":
+        print("Mycket bra val")
+        hallen()
+
+    if frukost == "1":
+        global droger
+        if droger != ["gas"]:
+            droger = droger + ["gas"]
+            print(f"du har tagit drogerna {droger}")
+
+        while frukost == "1":
+            global tunnan
+            tunnan = input("""
+            Hela världen snurrar och du kan inte skilja upp och ned, det känns ganska bra.
+            för:                    skriv:
+            Quit while youre ahead:   1
+            Drick gift (som en idiot) 2
+            """)
+
+            if tunnan == "1":
+                köket()
+            if tunnan == "2":
+                global spelare
+                if spelare != ["cancer"]:
+                    spelare = spelare + ["cancer"]
+                    print(f"du har {spelare}")
+                hallen()
+
+# hallen
+def hallen():
+    global dagsplan
+    dagsplan = input("""
+        du är mätt och går till hallen för att fundera på vad du vill göra idag
+        för:            skriv:
+        gå till skolan:   1
+        gå ut på äventyr: 2
+        stanna hemma:     3
+        """)
+
+    if dagsplan == "3":
+        print("Du stannar hemma och chillar hela dagen")
+        val = input("""
+            Ending: Gött!
+            för:    skriv:
+            börja om: 0
+            """)
+        if val == "0":
+            start()
+
+    while dagsplan == 1:
+        global skolval
+        skolval = input("""
+        Det är söndag... skolan är stängd.
+        Vill du försöka ta dig in ändå?
+        för:       skriv:
+        inbrott:     1
+        gå hem igen: 2
+        """)
+        if skolval == "2":
+            hallen()
+        if skolval == "1":
+            Skolan()
 
 #dra igång allt
 start()
